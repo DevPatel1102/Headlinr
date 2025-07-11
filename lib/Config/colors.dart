44e2a21297914762ee1644fd8122675176a39eab
DevPatel <devpateljp11@gmail.com>
2025-07-11 18:01:58 +0530

    Download App Added in Readme

--- lib/Config/colors.dart
+++ lib/Config/colors.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const lightBgColor = Color(0xffFFFFFF);
 const lightFontColor = Color(0xff191B28);
 const lightDivColor = Color(0xfff3f3f3);
-const lightPrimaryColor = Color(0xff5a90f8);
+const lightPrimaryColor = Color(0xff578FCA);
 const lightLabelColor = Color(0xff8E8E8E);
 const lightBottomNavColor = Color(0xfff8f8f8);
 
@@ -12,7 +12,7 @@ const lightBottomNavColor = Color(0xfff8f8f8);
 const darkBgColor = Color(0xff191B28);
 const darkFontColor = Color(0xffFFFFFF);
 const darkDivColor = Color(0xff34374B);
-const darkPrimaryColor = Color(0xffFF9900);
+const darkPrimaryColor = Color(0xffb87e02);
 const darkLabelColor = Color(0xff9E9E9E);
 const darkBottomNavColor = Color(0xff1f2233);
 
